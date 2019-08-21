0e15fd56ad252aef63fbf487d3dc1a23818c2d67
Jeremy Foster <jeremy.foster@live.com>
2019-08-20 22:11:46 -0700

    finish and simplify

--- sender.ts
+++ sender.ts
@@ -1,46 +1,26 @@
-import { ServiceBusClient, SendableMessageInfo } from "@azure/service-bus";
+import { ServiceBusClient } from "@azure/service-bus";
 
-// Define connection string and related Service Bus entity names here
 const connectionString = "Endpoint=sb://exrxsb.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=kppcYvwJEC5XoJeHPzKBYbxrOeb6zxccU6tpqFrWRAU=";
 const topicName = "messages";
+const sbClient = ServiceBusClient.createFromConnectionString(connectionString);
+const topicClient = sbClient.createTopicClient(topicName);
+const sender = topicClient.createSender();
 
-const listOfScientists = [
-    { name: "Einstein", firstName: "Albert" },
-    { name: "Heisenberg", firstName: "Werner" },
-    { name: "Curie", firstName: "Marie" },
-    { name: "Hawking", firstName: "Steven" },
-    { name: "Newton", firstName: "Isaac" },
-    { name: "Bohr", firstName: "Niels" },
-    { name: "Faraday", firstName: "Michael" },
-    { name: "Galilei", firstName: "Galileo" },
-    { name: "Kepler", firstName: "Johannes" },
-    { name: "Kopernikus", firstName: "Nikolaus" }
-];
+let i = 0;
+setInterval(() => {
+    let messageId = i + 1;
+    let carId = (i % 50) + 1;
 
-async function main(): Promise<void> {
-    const sbClient = ServiceBusClient.createFromConnectionString(connectionString);
-
-    const topicClient = sbClient.createTopicClient(topicName);
-    const sender = topicClient.createSender();
-
-    try {
-        for (let index = 0; index < listOfScientists.length; index++) {
-            const scientist = listOfScientists[index];
-            const message: SendableMessageInfo = {
-                body: `${scientist.firstName} ${scientist.name}`,
-                label: "Scientist"
-            };
-
-            console.log(`Sending message: ${message.body} - ${message.label}`);
-            await sender.send(message);
+    sender.send({
+        sessionId: `${carId}`,
+        userProperties: {
+            carId: carId
+        },
+        body: {
+            createTimestamp: Date.now(),
+            messageId: messageId
         }
-
-        await topicClient.close();
-    } finally {
-        await sbClient.close();
-    }
-}
-
-main().catch((err) => {
-    console.log("Error occurred: ", err);
-});
+    });
+    console.log(`Sending message ${messageId} (car ${carId})`);
+    i++;
+}, 2)
